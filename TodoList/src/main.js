18b003550c28a975637197420176505d21e3a8ef
raopremrao <raom61337@gmail.com>
2025-08-15 02:02:11 +0530

    About section

--- TodoList/src/main.js
+++ TodoList/src/main.js
@@ -1,9 +1,19 @@
 import gsap from 'gsap';
-import { SplitText } from 'gsap/all';
-import { ScrollTrigger } from 'gsap/all';
+import { SplitText } from 'gsap/SplitText';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Lenis from "lenis";
 
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    const lenis = new Lenis();
+    lenis.on('scroll', ScrollTrigger.update);
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000); 
+    });
+    gsap.ticker.lagSmoothing(0);
+
+
     gsap.registerPlugin(ScrollTrigger);
 
 var menuIcon = document.querySelector('.menu-icon');
@@ -70,6 +80,70 @@ gsap.from("#Home .word7", {
     opacity: 0,
     stagger: 0.3,
 })
+
+gsap.utils.toArray("#aboutA").forEach((item) => {
+    const img = item.querySelector(".about_image");
+    const nameH1 = item.querySelector(".about_name h1");
+
+    const splitH1 = SplitText.create(nameH1, {
+        type: "chars",
+        mask: "chars"
+    })
+
+    gsap.set(splitH1.chars, {
+        y: "125%"
+    });
+
+    splitH1.chars.forEach((char, index) => {
+        ScrollTrigger.create({
+            trigger: item,
+            start: `top+=${index * 25 - 250} top`,
+            end: `top+=${index * 25 -100} top`,
+            scrub: 1,
+            animation: gsap.fromTo(
+                char, {
+                    y: "125%"
+                },{
+                    y: "0%",
+                    ease: "none",
+                }
+            )
+        })
+    })
+
+    ScrollTrigger.create({
+        trigger: item,
+        start: "top bottom",
+        end: "top top",
+        scrub: 0.5,
+        // markers: true,
+        animation: gsap.fromTo(
+            img, {
+                clipPath: "polygon(25% 25%, 75% 40%, 100% 100%, 0% 100%)"
+            }, {
+                clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+                ease: "none"
+            }
+        )
+    })
+
+    ScrollTrigger.create({
+        trigger: item,
+        start: "bottom bottom",
+        end: "bottom top",
+        scrub: 0.5,
+        // markers: true,
+        animation: gsap.fromTo(
+            img, {
+                clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
+            }, {
+                clipPath: "polygon(0% 0%, 100% 0%, 75% 60%, 25% 75%)",
+                ease: "none"
+            }
+        )
+    })
+})
+
 
 
 
